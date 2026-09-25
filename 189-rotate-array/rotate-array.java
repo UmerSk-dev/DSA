3f2cec1d36f17656e5682e5f51b03026ec1d80b4
Umer <studentjmct@gmail.com>
2026-09-25 11:32:02 +0530

    Time: 4 ms (67.38%) | Memory: 268.9 MB (24.12%) - LeetSync

--- 189-rotate-array/rotate-array.java
+++ 189-rotate-array/rotate-array.java
@@ -13,6 +13,7 @@ class Solution {
         if (k < 0) {
             k = k + nums.length;
         }
+        
     reverse(nums,nums.length - k,nums.length-1);
     reverse(nums,0, nums.length - k - 1);
     reverse(nums,0,nums.length-1);
